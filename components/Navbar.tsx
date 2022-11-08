ee77db8880101f999c74aefa5e7f8a9c19f21157
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-11-08 18:37:40 +0100

    fix: key on navigation items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
-import useAuthentification from '../hooks/useAuthentification';
-import { useAuth } from '../hooks/useAuth';
-import { LOGGED_NAVIGATION, NOT_LOGGED_NAVIGATION } from '../constants';
 import Link from 'next/link';
+
+import { useAuth } from '../hooks/useAuth';
+
+import { LOGGED_NAVIGATION, NOT_LOGGED_NAVIGATION } from '../constants';
 
 const NavigationItem: React.FC<{
   url: string;
@@ -20,10 +21,18 @@ const Navbar: React.FC = () => {
         <ul className="flex items-center justify-center h-full">
           {user
             ? LOGGED_NAVIGATION.map((item) => (
-                <NavigationItem url={item.URL} label={item.NAME} />
+                <NavigationItem
+                  key={item.URL}
+                  url={item.URL}
+                  label={item.NAME}
+                />
               ))
             : NOT_LOGGED_NAVIGATION.map((item) => (
-                <NavigationItem url={item.URL} label={item.NAME} />
+                <NavigationItem
+                  key={item.URL}
+                  url={item.URL}
+                  label={item.NAME}
+                />
               ))}
         </ul>
       </nav>
